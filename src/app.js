c5abdcacf7e70ca3ccafa038788e068512ff0394
cpecoraro18 <cpecoraro18@gmail.com>
2020-10-06 16:26:19 -0500

    added support for ejs, mysql, and model/view/controller design structure, added example routes and pages

--- src/app.js
+++ src/app.js
@@ -1,21 +1,43 @@
 var express = require('express');
 var path = require('path');
-const bodyparser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var exampleRouter = require('./routes/example')
 
 var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views/pages'));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tasks', exampleRouter)
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// function to return the 404 message and error to client
+app.get('*', function(req, res, next) {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error)
+});
+
+app.use(function(error, req, res, next) {
+  res.status(error.status || 500);
+  res.json({
+    error : {
+      message: error.message
+    }
+  });
+  console.log("ERROR: ", error);
+});
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {page:'Home', menuId:'home'});
 });
-
+router.get('/about', function(req, res, next) {
+  res.render('about', {page:'About', menuId:'about'});
+});
 module.exports = router;
