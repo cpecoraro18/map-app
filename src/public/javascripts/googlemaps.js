4c8f3d106a4a6555ffb80bf6b01bcb2e50ac884a
cpecoraro18 <cpecoraro18@gmail.com>
2020-10-19 12:41:04 -0500

    added sidebar and other pages

--- src/public/javascripts/googlemaps.js
+++ src/public/javascripts/googlemaps.js
@@ -1,6 +1,6 @@
 //add google maps script to html page
 document.addEventListener('DOMContentLoaded', function () {
-  if (document.querySelectorAll('#googleMap').length > 0) {
+  if (document.querySelectorAll('#map').length > 0) {
     if (document.querySelector('html').lang)
       lang = document.querySelector('html').lang;
     else
@@ -26,5 +26,71 @@ function initMap() {
   fullscreenControl: true
 };
 
-  map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  map = new google.maps.Map(document.getElementById("map"), mapProp);
+  const leftControlDiv = document.createElement("div");
+  LeftControl(leftControlDiv, map);
+  map.controls[google.maps.ControlPosition.LEFT_CENTER].push(leftControlDiv);
+
+  const rightControlDiv = document.createElement("div");
+  RightControl(rightControlDiv, map);
+  map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(rightControlDiv);
+
+
+
+}
+
+function LeftControl(controlDiv, map) {
+  // Set CSS for the control border.
+  const controlUI = document.createElement("div");
+  controlUI.style.backgroundColor = "#fff";
+  controlUI.style.border = "2px solid #fff";
+  controlUI.style.borderRadius = "3px";
+  controlUI.style.boxShadow = "0 2px 6px rgba(0,0,0,.3)";
+  controlUI.style.cursor = "pointer";
+  controlUI.style.margin = "30px";
+  controlUI.style.textAlign = "center";
+  controlUI.title = "Click to recenter the map";
+  controlDiv.appendChild(controlUI);
+  // Set CSS for the control interior.
+  const controlText = document.createElement("div");
+  controlText.style.color = "rgb(25,25,25)";
+  controlText.style.fontFamily = "Roboto,Arial,sans-serif";
+  controlText.style.fontSize = "16px";
+  controlText.style.lineHeight = "38px";
+  controlText.style.paddingLeft = "5px";
+  controlText.style.paddingRight = "5px";
+  controlText.innerHTML = '<i class="fas fa-arrow-left"></i>';
+  controlUI.appendChild(controlText);
+  // Setup the click event listeners: simply set the map to Chicago.
+  controlUI.addEventListener("click", () => {
+    map.setCenter(chicago);
+  });
+}
+
+function RightControl(controlDiv, map) {
+  // Set CSS for the control border.
+  const controlUI = document.createElement("div");
+  controlUI.style.backgroundColor = "#fff";
+  controlUI.style.border = "2px solid #fff";
+  controlUI.style.borderRadius = "3px";
+  controlUI.style.boxShadow = "0 2px 6px rgba(0,0,0,.3)";
+  controlUI.style.cursor = "pointer";
+  controlUI.style.margin = "30px";
+  controlUI.style.textAlign = "center";
+  controlUI.title = "Click to recenter the map";
+  controlDiv.appendChild(controlUI);
+  // Set CSS for the control interior.
+  const controlText = document.createElement("div");
+  controlText.style.color = "rgb(25,25,25)";
+  controlText.style.fontFamily = "Roboto,Arial,sans-serif";
+  controlText.style.fontSize = "16px";
+  controlText.style.lineHeight = "38px";
+  controlText.style.paddingLeft = "5px";
+  controlText.style.paddingRight = "5px";
+  controlText.innerHTML = '<i class="fas fa-arrow-right"></i>';
+  controlUI.appendChild(controlText);
+  // Setup the click event listeners: simply set the map to Chicago.
+  controlUI.addEventListener("click", () => {
+    map.setCenter(chicago);
+  });
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,23 @@ const {ensureAuthenticated, forwardAuthenticated} = require('../config/auth');
 
 /* GET home page. */
 router.get('/', ensureAuthenticated, function(req, res, next) {
-  res.render('index', {page:'Home', menuId:'home', username: req.user.username});
+  res.render('home', {page:'Home', menuId:'home', username: req.user.username});
 });
-router.get('/about', function(req, res, next) {
-  res.render('about', {page:'About', menuId:'about'});
+router.get('/profile', ensureAuthenticated, function(req, res, next) {
+  res.render('profile', {page:'Profile', menuId:'profile', username: req.user.username});
+});
+router.get('/explore', ensureAuthenticated, function(req, res, next) {
+  res.render('explore', {page:'Explore', menuId:'explore', username: req.user.username});
+});
+router.get('/customizeMap', ensureAuthenticated, function(req, res, next) {
+  res.render('customizeMap', {page:'Customize', menuId:'Customize', username: req.user.username});
+});
+router.get('/settings', ensureAuthenticated, function(req, res, next) {
+  res.render('settings', {page:'Settings', menuId:'Settings', username: req.user.username});
+});
+router.get('/changePassword', ensureAuthenticated, function(req, res, next) {
+  const errors = req.flash().error || [];
+  res.render('changePassword', {page:'Customize', menuId:'Customize', username: req.user.username, errors: errors});
 });
 router.get('/login', forwardAuthenticated, function(req, res) {
   const errors = req.flash().error || [];
