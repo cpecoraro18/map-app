5c40ad6eaff117d84058860d44a847463616a648
Chris Pecoraro <cpecoraro18@gmail.com>
2020-12-09 16:28:46 -0600

    fixed dependencies

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const logger = require('morgan');
 const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
